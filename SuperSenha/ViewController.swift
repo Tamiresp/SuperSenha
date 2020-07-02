7d0ce08d609ad9c98e3b3f5f5eab9f284a19556d
Tamires Pereira <tamires.p.silva@accenture.com>
2020-07-02 16:23:19 -0300

    Super Senha

--- SuperSenha/ViewController.swift
+++ SuperSenha/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  SuperSenha
+//  aa
 //
 //  Created by tamires.p.silva on 01/07/20.
 //  Copyright © 2020 Tamires. All rights reserved.
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tfTotalPasswords: UITextField!
+    @IBOutlet weak var tfNumberOfCharacters: UITextField!
+    @IBOutlet weak var swLetters: UISwitch!
+    @IBOutlet weak var swNumbers: UISwitch!
+    @IBOutlet weak var swCaptitalLetters: UISwitch!
+    @IBOutlet weak var swSpecialCharacters: UISwitch!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let passwordsViewController = segue.destination as! PasswordViewController
+        
+        // forma mais segura (usar if let)
+        if let numberOfPasswords = Int(tfTotalPasswords.text!) {
+            // se conseguir obter o valor do campo e converter para inteiro
+            passwordsViewController.numberOfPasswords = numberOfPasswords
+        }
+        if let numberOfCharacters = Int(tfNumberOfCharacters.text!) {
+            passwordsViewController.numberOfCharacters = numberOfCharacters
+        }
+        passwordsViewController.useNumbers = swNumbers.isOn
+        passwordsViewController.useCapitalLetters = swCaptitalLetters.isOn
+        passwordsViewController.useLetters = swLetters.isOn
+        passwordsViewController.useSpecialCharacters = swSpecialCharacters.isOn
+        
+        // forcar encerrar o modo de edicao // remove o foco e libera teclado
+        view.endEditing(true)
+    }
 
 }
 
